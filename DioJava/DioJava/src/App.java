57a5e57d270777be661dd1f105d6d54d1f207279
unknown <bcmoura@gmail.com>
2022-05-18 11:43:47 -0300

    adicionando novo projeto de java da Dio

---
+++ DioJava/DioJava/src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args)  {
+        int i;
+        //int i;
+        //int I;
+        int _1a;
+        int $eg;
+
+        i =5;
+        int ia = 8;
+        _1a = 99;
+        $eg = 89;
+
+        final int j = 10;
+        j = 15;
+        int uiujn = 8;
+        
+        System.out.println     
+
+
+
+    }
+
+    public App() {
+    }
+}
